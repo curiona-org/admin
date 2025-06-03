bdcfb43f0ddd1a9dc21c1c357adde09c460127f6
HotPotatoC <juandotulung@gmail.com>
2025-06-03 15:00:23 +0700

    feat: updated roadmap information style

--- components/roadmap-information.tsx
+++ components/roadmap-information.tsx
@@ -39,7 +39,8 @@ export function RoadmapInformation({ roadmap }: RoadmapInformationProps) {
 
   return (
     <div className='relative flex flex-col gap-4 overflow-auto px-4 lg:px-6'>
-      <h1 className='text-2xl font-semibold'>Roadmap Information</h1>
+      <h1 className='text-2xl font-semibold'>{roadmap.title}</h1>
+      <h4 className='text-lg text-muted-foreground'>Roadmap Information</h4>
       <Separator />
       <div className='flex flex-col gap-2'>
         <div className='grid grid-cols-1 md:grid-cols-2 py-2 gap-y-6 md:gap-y-12 gap-x-6 md:gap-x-12 lg:gap-x-32 xl:gap-x-52'>
